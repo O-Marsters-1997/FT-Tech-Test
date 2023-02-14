d9aeed7e2fe1311991f33b9e3b8c1fc0a7e29a21
Olly Marsters <olly.marsters@yahoo.co.uk>
2023-02-14 19:02:50 +0000

    setup basic prettier, eslint, and husky to run lint on lint-staged on pre-commit hook and modified webpack config for consistency

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,8 +1,9 @@
-const subject = require('../src/js/main');
+// Commenting out for now
+// const subject = require("../src/js/main");
 
 // replace this with your tests
-describe('Testing the client', () => {
-	it('works', () => {
-		expect(1).toBe(1);
-	});
+describe("Testing the client", () => {
+  it("works", () => {
+    expect(1).toBe(1);
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	mode: 'development',
-	devtool: 'inline-source-map',
-	entry: [
-		__dirname + '/src/css/styles.scss',
-		__dirname + '/src/js/main.js'
-	],
-	output : {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/[name].js',
-		publicPath: 'auto'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.m?jsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: { outputPath: 'css/', name: '[name].css'}
-					},
-					'sass-loader'
-				]
-			}
-		]
-	}
-}
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: [
+    path.resolve(__dirname, "/src/css/styles.scss"),
+    path.resolve(__dirname, "/src/js/main.js"),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: { outputPath: "css/", name: "[name].css" },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
