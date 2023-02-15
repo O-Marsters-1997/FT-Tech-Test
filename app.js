52c69a88c0fa8cb179f26d441bd8d120761cadba
Olly Marsters <olly.marsters@yahoo.co.uk>
2023-02-15 09:31:13 +0000

    changed axios to common module

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 3000;
 const { engine } = require("express-handlebars");
 // dotcom-server-react is a tool that enables us to run both handlebars and react in the same app. You don't need to spend time figuring out how it works.
 const { PageKitReactJSX } = require("@financial-times/dotcom-server-react-jsx");
+
+// adding in axios to make request to stock API
+const axios = require("axios");
+
 const jsxRenderer = new PageKitReactJSX();
 
 const app = express();
@@ -43,10 +47,20 @@ app.get("/jsx", (req, res) => {
  */
 
 app.get("/handlebars", async function (req, res) {
+  const BASE_URL =
+    "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes";
+  const params = ["FTSE:FSI", "INX:IOM", "EURUSD", "GBPUSD", "IB.1:IEU"];
+  const fetchData = async () => {
+    const result = await axios.get(`${BASE_URL}?symbols=${params.join(",")}`);
+    return result.data.data;
+  };
+  const data = await fetchData();
+  data.items.forEach((item) => console.log(item.basic.name));
+
   // This object is passed to the Handlebars template.
   const templateData = {
     pageTitle: "Home",
-    content: "Hello World!",
+    content: data,
   };
 
   // This renders the Handlebars view at `views/home.handlebars`.
---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  BASE_URL:
+    "https://markets-data-api-proxy.ft.com/research/webservices/securities/v1/quotes",
+  params: ["FTSE:FSI", "INX:IOM", "EURUSD", "GBPUSD", "IB.1:IEU"],
+};
+
+module.exports(config);
