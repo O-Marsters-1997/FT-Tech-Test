d60e321252bdc2934698fe5f629bca4b7633ede7
Olly Marsters <olly.marsters@yahoo.co.uk>
2023-02-17 10:35:03 +0000

    added initial tests to contineu test http status results

--- test/app.spec.js
+++ test/app.spec.js
@@ -6,5 +6,9 @@ describe("Testing the server", () => {
     const response = await request(subject).get("/");
     expect(response.statusCode).toBe(200);
   });
+  // add your own tests here
+  it("can run the express server and return a 404 for invalid url", async () => {
+    const response = await request(subject).get("/invalid");
+    expect(response.statusCode).toBe(404);
+  });
 });
-// add your own tests here
